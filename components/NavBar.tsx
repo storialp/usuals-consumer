a8bcb01e6c99a3f51e58f97003cca72b2fc31103
storialp <luis.pericchi@studbocconi.it>
2023-03-11 19:07:39 +0100

    Removed Navbar bug: focus property inside Dialog.Panel issue

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -41,7 +41,7 @@ export default function NavBar() {
           </nav>
           <Dialog as='div' open={mobileMenuOpen} onClose={setMobileMenuOpen}>
             <Dialog.Panel
-              focus='true'
+              // focus='true'
               className='fixed inset-0 z-10 overflow-y-auto bg-white px-6 py-6 lg:hidden'
             >
               <div className='flex h-9 items-center justify-between'>
