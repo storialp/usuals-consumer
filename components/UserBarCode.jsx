6cd57d87d74e22f0a2941eb8c1b9733e1f0ab251
storialp <luis.pericchi@studbocconi.it>
2023-03-12 23:51:40 +0100

    Added uuid to barcode

--- components/UserBarCode.jsx
+++ components/UserBarCode.jsx
@@ -4,5 +4,5 @@ import { useUser } from '@supabase/auth-helpers-react'
 export const UserBarCode = () => {
   const user = useUser()
   console.log(user)
-  return <Barcode value={user} />
+  return <Barcode value={user.id} />
 }
