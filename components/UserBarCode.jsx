ea8811ad3fca4815ca9ba5cdae0a186aacb95ce5
storialp <luis.pericchi@studbocconi.it>
2023-03-13 01:17:11 +0100

    Testing Navbar width

--- components/UserBarCode.jsx
+++ components/UserBarCode.jsx
@@ -5,7 +5,7 @@ export const UserBarCode = () => {
   const user = useUser()
   console.log(user)
   return (
-    <div className='flex items-center justify-center h-1/2 w-1/2'>
+    <div className='flex items-center justify-center h-1/2 w-48'>
       <Barcode value={user.id} />
     </div>
   )
