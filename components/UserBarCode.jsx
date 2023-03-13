fc7fa53fc55edf03284194e456e00f9b8f0803f4
storialp <luis.pericchi@studbocconi.it>
2023-03-13 19:09:22 +0100

    Testing translator

--- components/UserBarCode.jsx
+++ components/UserBarCode.jsx
@@ -1,10 +1,14 @@
 import Barcode from 'react-barcode'
 import { useUser } from '@supabase/auth-helpers-react'
-import { Translator } from 'short-uuid'
+const short = require('short-uuid')
 
 export const UserBarCode = () => {
   const user = useUser().id
+  console.log(user)
+  const translator = short()
   const shortID = translator.fromUUID(user)
+  const longID = translator.toUUID(shortID)
+  console.log(longID)
   return (
     <div className='flex items-center justify-center'>
       <Barcode value={shortID} width='1' />
