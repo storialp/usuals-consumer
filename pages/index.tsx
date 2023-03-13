c3a0a2a4ef5e52ff2592ff5b33acc3173d5c02b4
storialp <luis.pericchi@studbocconi.it>
2023-03-13 01:15:31 +0100

    Testing Navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,8 +17,7 @@ export default function Home() {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      {/* {session && <NavBar />} */}
-      <NavBar />
+      {session && <NavBar />}
       {!session ? <SignUp /> : <UserBarCode />}
     </>
   )
