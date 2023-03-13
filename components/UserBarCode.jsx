aa6a28f17f9dbb8b77d61e6ed7e5f88f89d6c12d
storialp <luis.pericchi@studbocconi.it>
2023-03-13 18:55:52 +0100

    Added UUID shortener

--- components/UserBarCode.jsx
+++ components/UserBarCode.jsx
@@ -1,12 +1,13 @@
 import Barcode from 'react-barcode'
 import { useUser } from '@supabase/auth-helpers-react'
+import { Translator } from 'short-uuid'
 
 export const UserBarCode = () => {
-  const user = useUser()
-  console.log(user)
+  const user = useUser().id
+  const shortID = translator.fromUUID(user)
   return (
     <div className='flex items-center justify-center'>
-      <Barcode value={user.id} width='1' />
+      <Barcode value={shortID.id} width='1' />
     </div>
   )
 }
