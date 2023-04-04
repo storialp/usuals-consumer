d3692210c3d8e03ef78f42e80261a389f2afc127
storialp <luis.pericchi@studbocconi.it>
2023-04-04 22:20:13 +0200

    Fixed infinite rerenders

--- components/MyBusinesses.jsx
+++ components/MyBusinesses.jsx
@@ -26,7 +26,7 @@ export const MyBusinesses = () => {
         console.log(result.data)
       })
     console.log(myPrograms)
-  }, [myPrograms, user])
+  }, [user])
   return (
     <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
