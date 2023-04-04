aa2564b640331c6e16c2ef31883e0b17e6ff3fc9
storialp <luis.pericchi@studbocconi.it>
2023-04-04 22:39:53 +0200

    Added external source (s3) to nextxonfig

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "personal-website-pics-2.s3.eu-central-1.amazonaws.com",
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
