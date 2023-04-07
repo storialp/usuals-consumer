ff95fc62500f87158dc5aa8f451d03af365f7335
storialp <luis.pericchi@studbocconi.it>
2023-04-07 22:26:32 +0200

    Moved away from shortuuid, too many problems

--- components/UserCodes.jsx
+++ components/UserCodes.jsx
@@ -1,31 +1,33 @@
-import { useUser } from '@supabase/auth-helpers-react'
-import { QRCodeSVG } from 'qrcode.react'
-const short = require('short-uuid')
-import ReactBarcode from './ReactBarCode'
-import YourUsualsCard from './YourUsualsCard'
+import { useUser } from "@supabase/auth-helpers-react"
+import { QRCodeSVG } from "qrcode.react"
+// const short = require('short-uuid')
+import ReactBarcode from "./ReactBarCode"
+import YourUsualsCard from "./YourUsualsCard"
 
 export const UserCodes = () => {
-  const user = useUser().id
+  const user = useUser()?.id
   console.log(user)
-  const translator = short(
-    '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
-  )
-  const shortID = translator.fromUUID(user)
-  console.log(shortID)
-  const longID = translator.toUUID(shortID)
-  console.log(longID)
+  // const translator = short(
+  //   '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+  // )
+  // const shortID = translator.fromUUID(user)
+  // console.log(shortID)
+  // const longID = translator.toUUID(shortID)
+  // console.log(longID)
   return (
-    <div className='flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8'>
-      <div className='sm:mx-auto sm:w-full sm:max-w-md'>
+    <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
         {/* <h2 className='mt-6 text-center text-3xl font-bold tracking-tight text-gray-900'>
           Your Card
         </h2> */}
-        <YourUsualsCard className='mx-auto w-2/5 h-auto' />
+        <YourUsualsCard className="mx-auto w-2/5 h-auto" />
       </div>
-      <div className='mt-8 mx-auto w-5/6 sm:w-full max-w-md'>
-        <div className='bg-white py-8 px-10 shadow rounded-lg sm:px-10'>
-          <QRCodeSVG value={shortID} className='mx-auto' />
-          <ReactBarcode value={shortID} className='w-full' />
+      <div className="mt-8 mx-auto w-5/6 sm:w-full max-w-md">
+        <div className="bg-white py-8 px-10 shadow rounded-lg sm:px-10">
+          <QRCodeSVG value={user} className="mx-auto" />
+          <ReactBarcode value={user} className="w-full" />
+          {/* <QRCodeSVG value={shortID} className='mx-auto' />
+          <ReactBarcode value={shortID} className='w-full' /> */}
           {/* <QRCodeSVG value='2UJNaZ0icRgeBVV74zIBos' className='mx-auto' />
           <ReactBarcode value='2UJNaZ0icRgeBVV74zIBos' className='w-full' /> */}
         </div>
