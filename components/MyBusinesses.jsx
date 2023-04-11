f611896ea1a416bbac8704cb063d94c126e17c82
storialp <pericchiluis@gmail.com>
2023-04-11 03:42:20 +0200

    trying different styling 10

--- components/MyBusinesses.jsx
+++ components/MyBusinesses.jsx
@@ -40,16 +40,16 @@ export const MyBusinesses = () => {
             {myPrograms.map((item) => (
               <Link href={`/businesses/${item.id}`} key={item.id}>
                 <div className="relative flex items-center space-x-3 rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm focus-within:ring-2 focus-within:ring-blue-500 focus-within:ring-offset-2 hover:border-gray-400">
-                  <div className="flex-shrink-0">
-                    <Image
-                      className="h-10 w-10 rounded-full object-cover"
-                      src={
-                        item.logo_url ||
-                        "https://personal-website-pics-2.s3.eu-central-1.amazonaws.com/icon+(2).png"
-                      }
-                      alt={item.business_name}
-                    />
-                  </div>
+                  {/* <div className="flex-shrink-0"> */}
+                  <Image
+                    className="h-10 w-10 rounded-full object-cover"
+                    src={
+                      item.logo_url ||
+                      "https://personal-website-pics-2.s3.eu-central-1.amazonaws.com/icon+(2).png"
+                    }
+                    alt={item.business_name}
+                  />
+                  {/* </div> */}
                   <div className="min-w-0 flex-1">
                     <span className="absolute inset-0" aria-hidden="true" />
                     <p className="text-sm font-medium text-gray-900">
