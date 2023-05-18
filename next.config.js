0fbb30c6d8c0b4a93ebdbb2780062d2cad23cd8e
storialp <luis.pericchi@studbocconi.it>
2023-05-18 17:28:51 +0200

    Allowing immages from my S3 bucket

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "personal-website-pics-2.s3.eu-central-1.amazonaws.com",
+      },
+    ],
+  },
+}
 
 module.exports = nextConfig
